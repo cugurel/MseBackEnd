451397ae075f58b0d6b0efeb97c4605455f02d26
Cagri Ugurel <cagr.ugurel@fink-telecom.com.tr>
2022-07-19 17:01:52 +0300

    aspects

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,41 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    public class MethodInterception : MetodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, Exception e) { }
+        protected virtual void OnSuccess(IInvocation İnvocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }catch(Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+
+            OnAfter(invocation);
+        }
+    }
+}
